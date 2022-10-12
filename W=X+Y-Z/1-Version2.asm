a5a24eb8783cd501f28b443616f75168f2c86c21
Nik41d0 <93205099+Nik41d0@users.noreply.github.com>
2022-10-12 20:20:55 +0800

    Create 1-Version2.asm

---
+++ W=X+Y-Z/1-Version2.asm
@@ -0,0 +1,107 @@
+
+DATAS SEGMENT
+    X DB ?
+    Y DB ?
+    Z DB ?
+    STR1 DB "PLEASE INPUT YOUR DATA",13,10,"$" 
+DATAS ENDS
+
+STACKS SEGMENT
+    
+STACKS ENDS
+
+CODES SEGMENT
+    ASSUME CS:CODES,DS:DATAS,SS:STACKS
+START:
+	MOV AX,DATAS
+	MOV DS,AX
+    MOV AH,9   
+    LEA DX,STR1 
+    INT  21H
+LP: 
+    MOV AH,2  
+    MOV DL,13
+    INT 21H 
+    MOV AH,2  
+    MOV DL,10
+    INT 21H
+    
+    MOV AH,1
+    INT 21H
+    CMP AL,27
+    JZ  EXIT   
+
+    SUB AL,30H
+    MOV BL,10
+    IMUL BL
+    MOV X,AL
+   
+    MOV AH,1
+    INT 21H
+    SUB AL,30H
+    ADD X,AL  
+
+    MOV AH,2
+    MOV DL,'+'
+    INT 21H
+
+    MOV AH,1
+    INT 21H
+    SUB AL,30H
+
+    MOV BL,10
+    IMUL BL
+    MOV Y,AL
+
+    MOV AH,1
+    INT 21H
+    SUB AL,30H
+    ADD AL,Y
+    ADD X,AL  
+
+	MOV AH,2
+	MOV DL,"-"
+	INT 21H 
+	
+	MOV AH,1
+    INT 21H
+    SUB AL,30H
+
+    MOV BL,10
+    IMUL BL
+    MOV Z,AL
+
+    MOV AH,1
+    INT 21H
+    SUB AL,30H
+    ADD AL,Z  
+    SUB X,AL 
+
+    MOV AH,2
+    MOV DL,'='
+    INT 21H    
+    
+    MOV AH,0   
+    MOV AL,X
+    MOV BL,10 
+    IDIV BL
+    ADD AX,3030H
+    MOV BX,AX
+
+    MOV AH,2
+    MOV DL,BL
+    INT 21H 
+
+    MOV AH,2
+    MOV DL,BH
+    INT 21H
+         
+    JMP  LP
+EXIT: 
+    MOV AH,4CH
+    INT 21H  
+    
+    MOV AH,4CH
+    INT 21H
+CODES ENDS
+    END START
